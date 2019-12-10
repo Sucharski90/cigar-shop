89b14c1569d1bc017dde3ab17bbc72387ccb724d
Matt <MVSucharski@gmail.com>
2019-12-09 20:08:01 -0500

    finshed

---
+++ src/Components/Navbar.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import {ButtonContainer} from './Button' 
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <NavWrapper className="navbar navber-expand-sm navbar-dark px-sm-5">
+        <Link to='/'>
+          <i className="fas fa-joint navbar-brand"></i>
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to='/' className="nav-link">
+              Products
+      </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-shopping-cart">cart</i>
+            </span>
+          </ButtonContainer>
+        </Link>
+      </NavWrapper>
+    )
+  }
+}
+
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link{
+  color: var(--mainWhite) !important;
+  font-size:1.3rem;
+  text-transform: capitalize;
+
+}
+`
---
+++ src/data.js
@@ -0,0 +1,112 @@
+
+export const storeProducts = [
+    {
+      id: 1,
+      title: "KUBA KUBA",
+      img: "img/acid.png",
+      price: 193,
+      company: "Drew Estate",
+      info:
+        "Acid Kuba Kuba cigars were the first among the Acid cigars to be embraced by cigar smokers across the country. Although a bear hug would probably be more like it: THE standard-bearer for far-from-ordinary smokes, it's Kuba Kuba that's led the charge as the top choice among infused cigars. Handmade in one size, a plump 5 x 54 Robusto rolled in a dark, oily and virtually seamless Indonesian Sumatra wrapper, this hour-plus smoke comes with all the bona fides. Brimming with sweet, head-turning aromas and rich Nicaraguan spice, Kuba Kuba's ultra-smooth smoke has a unique taste and aroma. Then Drew Estate one-ups the game, imparting big flavor and bigger aroma to Drew Estates' finest filler tobaccos via a sweet, botanical concoction derived from a mix of over 140 herbs and essential oils.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 2,
+      title: "AVE MARIA",
+      img: "img/ava.png",
+      price: 129,
+      company: "AJ Fernandez",
+      info:
+        "",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 3,
+      title: "New World",
+      img: "img/new.png",
+      price: 99,
+      company: "AJ Fernandez",
+      info:
+        "",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 4,
+      title: "EL GUEGUENSE",
+      img: "img/wise.png",
+      price: 240,
+      company: "Foundation",
+      info:
+        "Pronounced El Whey-When-Say, Foundation Cigars' El Gueguense translates to The Wiseman in Nicaraguan culture. Handcrafted by AJ Fernandez, these cigars were the initial foray into the cigar market for Nick Melillo. This Robusto cigar actually took the #3 spot on Cigar Aficionado's 2018 Top 25 Cigars of the year with its San Andres Maduro wrapper that features notes cocoa, coffee, and sweet, creamy spices atop a Nicaraguan cast of long-filler tobaccos.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 5,
+      title: "XTRA RARE COLLECTION",
+      img: "img/art.png",
+      price: 109,
+      company: "ARTURO FUENTE",
+      info:
+        "Though they aren't always readily available, Arturo Fuente cigars sell fast when they're in stock. With patience and painstaking handcraftsmanship, the famed Fuente family has been producing affordably-priced cigars with outstanding quality for generations. Blended with tobaccos grown exclusively on the family's estate farms in the Dominican Republic, expect a cigar that's always rich in taste, well-balanced, and satisfying. If you haven't yet had the pleasure of these cigars, do so now!",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 6,
+      title: "BROTHERS OF THE LEAF",
+      img: "img/drew.png",
+      price: 131,
+      company: "Drew Estate",
+      info:
+        "Cigars and tobacco are seldom subjected to the kinds of mad-scientist experimentation employed by Drew Estate. From traditional to infused to gourmet-blended, their cigars offer something for everybody. Here is where you'll find a collection of Drew Estate samplers, offering you the chance to experience a truly unique smoke: cigars re-imagined. Try some today",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 7,
+      title: "AMERICAN BARREL AGED",
+      img: "img/cam.png",
+      price: 240,
+      company: "Camacho",
+      info:
+        "Camacho has distilled their most complex cigar yet! Camacho American Barrel Aged is a tasty, toasty smoke loaded with equal parts flavor, body and good ol' fashioned American ingenuity. Handcrafted of USA-grown tobaccos, the Barrel Aged secret sauce is a just-right pinch of Camacho's legendary Corojo leaf that's been aged 6 years, then locked down tight in charred Kentucky bourbon barrels and extra-aged to yield a cigar that drips with warm, lingering flavor. A must-buy - get your box today!",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 8,
+      title: "CONNECTICUT 7 X 70",
+      img: "img/asy.png",
+      price: 2,
+      company: "ASYLUM",
+      info:
+        "",
+      inCart: false,
+      count: 0,
+      total: 0
+    }
+  ];
+  
+  export const detailProduct = {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/camacho.png",
+    price: 10,
+    company: "google",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  };
