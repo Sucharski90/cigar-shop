acb4c0aaa0752c1d6f15a73b7daacbeba460a420
Matt <MVSucharski@gmail.com>
2019-12-10 22:05:15 -0500

    finshed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Components/Navbar";
@@ -15,12 +15,12 @@ class App extends Component {
       <React.Fragment>
         <Navbar />
         
-        
-          <Route exact path={process.env.PUBLIC_URL + '/'} component={ProductList} />
+        <Switch>
+          <Route path={process.env.PUBLIC_URL + '/'} component={ProductList} />
           <Route path="/details" component={Details} />
           <Route path="/cart" component={Cart} />
-          
-          
+          <Route component={Default} />
+        </Switch>
         
         <Modal />
       </React.Fragment>
