aff4286775895f30d5b6216627d8ff79fc2e0bf4
Matt <MVSucharski@gmail.com>
2019-12-12 20:17:27 -0500

    finshed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -12,7 +12,7 @@ export default class Navbar extends Component {
         </Link>
         <ul className="navbar-nav align-items-center">
           <li className="nav-item ml-5">
-            <Link to="{process.env.PUBLIC_URL + '/'}" className="nav-link">
+            <Link to="/cigar-shop" className="nav-link">
               Products
       </Link>
           </li>
