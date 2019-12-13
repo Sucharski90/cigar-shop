5ad262237dc697a1ea91bc0e538c401c1c2f6763
Matt <MVSucharski@gmail.com>
2019-12-12 22:05:03 -0500

    finshed

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -34,7 +34,7 @@ export default class Details extends Component {
                                  </p>
                                     <p className="text-muted lead">{info}</p>
                                     <div>
-                                        <Link to="/">
+                                        <Link exact to="/cigar-shop">
                                             <ButtonContainer>
                                                 back to list
                                          </ButtonContainer>
