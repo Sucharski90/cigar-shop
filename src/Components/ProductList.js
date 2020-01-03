3b6d853d4bb07cf41b384542d0598ec7d94c50c9
Matt <MVSucharski@gmail.com>
2020-01-02 20:12:01 -0500

    adding styles

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -13,7 +13,7 @@ export default class ProductList extends Component {
                 <div className="py-5">
                     <div className="container mt-5">
                         <div className="row">
-                        <div className="col-md-8">
+                        <div className="col-md-9">
                         <Title name="Featured" title="Smokes" />
                         <img className="ad img-fluid" src="https://i1.wp.com/www.casasfumando.com/wp-content/uploads/2018/04/Famous-Smoke-Shop-Ad-700-by-150.jpg?resize=620%2C133" alt=""/>
                             <ProductConsumer>
@@ -24,7 +24,7 @@ export default class ProductList extends Component {
                                 }}
                             </ProductConsumer>
                             </div>
-                            <div className="col-md-4">
+                            <div className="col-md-3">
                             <Utility />
                                
                             </div>
